b088d63fc4c28fcf64d5bfd73c071c2452b46383
Kaviya S <kaviya.s0806@gmail.com>
2025-06-14 11:11:00 +0530

    Create SumOfAllDigitIntoSingleDigit.java

---
+++ NumberBased/SumOfAllDigitIntoSingleDigit.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+import java.util.*;
+
+// Read only region start
+
+class UserMainCode
+
+{
+
+public int digitSum(int input1) {
+
+// Read only region end I
+
+// Write code here...
+
+boolean isNegative input1 < 10;
+
+input1 = Math.abs(input1);
+
+while(input1 >= 10){
+
+int sum = 0;
+
+while(input1 > 0){
+
+int digit = input1 % 10;
+
+sum += digit;
+while(input1 > 0){
+
+int digit input1% 10;
+
+sum += digit;
+
+input1 /= 10;
+
+}
+
+input1 = sum;
+
+}
+
+return isNegative ? -input1: input1;
+}
+}
